957b5f59181aa87c477f5a91c806be58b98b7643
Rajmohan Kathiresan <rajmohankathiresan@gmail.com>
2018-08-24 08:57:37 +0530

    Added aendpoint with simple mathematical functions

---
+++ MathDemo/SimpleCalculator/Controllers/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    public class Calculator : Controller
+    {
+        [Route("add")]
+        [HttpGet]
+        public int add(int a, int b) 
+        {
+            return a + b;
+        }
+
+        [Route("sub")]
+        [HttpGet]
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        [Route("mul")]
+        [HttpGet]
+        public int multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        [Route("div")]
+        [HttpGet]
+        public float divide(int a, int b)
+        {
+            return a/b;
+        }
+    }
+}
